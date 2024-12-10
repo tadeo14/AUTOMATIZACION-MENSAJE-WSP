c2fbfcd9332fa491b3a445b6e6373e066c75826e
tadeo trabajo <tadeoacosta14@gmail.com>
2024-12-10 19:44:01 -0300

    SCRIPT

---
+++ script.py
@@ -0,0 +1,10 @@
+import pywhatkit as kit
+
+#numero_tel = input("Ingrsa numero de telefono: ")*/
+
+
+#kit.sendwhatmsg("+5493424392941", "Mensaje test", 19, 15, 8, True, 5)
+
+#Grupo
+
+kit.sendwhatmsg_to_group("BY0FMqVMjEX8ualOUwDpbH", "Hola grupo",19, 23, 15, True, 5)
