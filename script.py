6046dbcbc48596905a73a505e0e784f336e8e4bd
tadeo trabajo <tadeoacosta14@gmail.com>
2024-12-21 11:23:58 -0300

    importe una libreria para que pueda recibir imagenes

--- script.py
+++ script.py
@@ -1,10 +1,20 @@
 import pywhatkit as kit
+import pyautogui
+import time
 
-#numero_tel = input("Ingrsa numero de telefono: ")*/
+# Enviar un mensaje directo
+kit.sendwhatmsg("+5493424392941", "numero tel", "numero tel" "Mensaje test", 11, 18)
 
+# Aumentar el tiempo de espera para que WhatsApp Web cargue correctamente
+time.sleep(25)
 
-#kit.sendwhatmsg("+5493424392941", "Mensaje test", 19, 15, 8, True, 5)
+# Ruta de la imagen que quieres enviar
+image_path = r"C:\Archivos Flexxus\PROMO.jpg"
 
-#Grupo
+# Automáticamente carga la imagen en el chat
+pyautogui.write(image_path)  # Escribe la ruta de la imagen
+pyautogui.press('enter')  # Presiona Enter para cargar la imagen
+time.sleep(2)  # Espera un momento para que se cargue la vista previa
 
-kit.sendwhatmsg_to_group("BY0FMqVMjEX8ualOUwDpbH", "Hola grupo",19, 23, 15, True, 5)
+# Envía la imagen
+pyautogui.press('enter')  # Presiona Enter para enviar
