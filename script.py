70daffa94997c89fd73e18f1da7bc1297034ab63
tadeo trabajo <tadeoacosta14@gmail.com>
2024-12-26 17:48:55 -0300

    mejore el codigo

--- script.py
+++ script.py
@@ -1,20 +1,13 @@
 import pywhatkit as kit
-import pyautogui
-import time
+from important import MobileN
 
 # Enviar un mensaje directo
-kit.sendwhatmsg("+5493424392941", "numero tel", "numero tel" "Mensaje test", 11, 18)
+#kit.sendwhatmsg("+5493424392941", "Mensaje test", 10, 25)
 
-# Aumentar el tiempo de espera para que WhatsApp Web cargue correctamente
-time.sleep(25)
+imagePath = "PROMO.jpg" 
+Caption = "a tumble"
 
-# Ruta de la imagen que quieres enviar
-image_path = r"C:\Archivos Flexxus\PROMO.jpg"
 
-# Automáticamente carga la imagen en el chat
-pyautogui.write(image_path)  # Escribe la ruta de la imagen
-pyautogui.press('enter')  # Presiona Enter para cargar la imagen
-time.sleep(2)  # Espera un momento para que se cargue la vista previa
-
-# Envía la imagen
-pyautogui.press('enter')  # Presiona Enter para enviar
+# Iterar sobre la lista de números y enviar el mensaje
+for numero in MobileN:
+    kit.sendwhats_image(numero, imagePath, Caption, 20)
