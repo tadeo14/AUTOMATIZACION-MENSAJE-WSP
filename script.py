2a435080b3f9cbdc93dda49826658a904d6344c1
tadeo trabajo <tadeoacosta14@gmail.com>
2024-12-28 09:12:54 -0300

    se hizo un codigo mas limpio y ordenado

--- script.py
+++ script.py
@@ -1,22 +1,21 @@
 import pywhatkit as kit
 import time
-from important import MobileN
-
-def enviar_mensaje(numero, imagen, caption, hora):
-    try:
-        kit.sendwhats_image(numero, imagen, caption, hora,False)
-        print(f"Mensaje enviado a {numero}")
-    except Exception as e:
-        print(f"Error al enviar mensaje a {numero}: {str(e)}")
-
-# Lista de números de teléfono
-#MobileN = ["+5493424392941", "+5493426102045"]
-
-imagePath = "PROMO.jpg"
-Caption = "Es una prueba, no contestar"
 
 
-# Iterar sobre la lista de números y enviar el mensaje
-for numero in MobileN:
-    enviar_mensaje(numero, imagePath, Caption, 20, 20)  # Ajusta la hora y minutos según sea necesario
-    time.sleep(120)  # Pausa de 30 segundos entre cada envío
+#Numero de la persona a enviar
+phone_number = ["+5493455081206","+5493426356915","+5493424798052","+5493425114189"]
+
+#Mensaje a enviar 
+message = "Hola, esto es otra prueba"
+
+#Archivo adjunto 
+hour=8,
+minute=52
+attachment = "PROMO.jpg"
+
+
+#Enviamos el mensaje 
+for phone_number in phone_number:
+
+    kit.sendwhats_image(phone_number,attachment,message,15,True)
+    time.sleep(10)
